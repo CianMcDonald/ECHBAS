6ee17c5e67d9d6eca3ac31d0fa018fc83a93bddb
louissullivan4 <sullivanlouis0@gmail.com>
2021-10-18 21:41:07 +0100

    updates reader

--- hospitalsystem/qrreader.py
+++ hospitalsystem/qrreader.py
@@ -2,9 +2,7 @@ from PIL import Image
 from pyzbar.pyzbar import decode
 data = decode(Image.open("static/qr_photos/qrdata.png"))
 new = data[0].data
-print(new)
 new = new.decode('utf-8')
 letter_list = new.split(",")
 print(letter_list)
-
 
