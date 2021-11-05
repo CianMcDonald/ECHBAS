c96bef9285b5388645f453d005ab8c0675ea9c22
CianMcDonald <cianmcdonald2001@gmail.com>
2021-11-05 00:57:55 +0000

    Changed font, added confirmations

--- hospitalsystem/home.py
+++ hospitalsystem/home.py
@@ -1,5 +1,7 @@
 import tkinter as tk
 from tkinter import font
+from tkinter import messagebox
+
 from patientform import run_patientform
 from bed_priority_queue import PriorityHeap
 from assign_bed import AssignBed
@@ -15,6 +17,7 @@ class HomePage(tk.Tk):
         self.geometry('1000x1000')
         self.titlefont = font.Font(family = 'Verdana', size=30,
                                     weight="bold")
+        self.textfont = font.Font(family = 'Verdana', size=15)
         self.title("Home Page")
         self.display_homepage()
         self.queue = PriorityHeap()
@@ -22,26 +25,28 @@ class HomePage(tk.Tk):
 
         
     def display_homepage(self):
-        title_label = tk.Label(self, text="Home", font=self.titlefont)
+        title_label = tk.Label(self, text="System Home", font=self.titlefont)
         title_label.place(relx=0.15, rely=0.1, relwidth=0.7, relheight=0.2)
         #title_label.pack()
 
-        calculate_triage_button = tk.Button(self, text="Add Patient", command = lambda: self.display_patientform(), bg="grey")
+        calculate_triage_button = tk.Button(self, text="Add Patient", command = lambda: self.display_patientform(), bg="grey", font=self.textfont)
         calculate_triage_button.place(relx=0.15, rely=0.3, relwidth=0.7, relheight=0.15)
         #calculate_triage_button.pack()
 
-        allocate_bed_button = tk.Button(self, text="Assign Bed to Patient", bg="grey", command=lambda: self.display_assignbed())
+        allocate_bed_button = tk.Button(self, text="Assign Bed to Patient", bg="grey", command=lambda: self.display_assignbed(), font=self.textfont)
         allocate_bed_button.place(relx=0.15, rely=0.5, relwidth=0.7, relheight=0.15)
 
-        edit_queue_button = tk.Button(self, text="Edit Queue", bg="grey")
+        edit_queue_button = tk.Button(self, text="Edit Queue", bg="grey", font=self.textfont)
         edit_queue_button.place(relx=0.15, rely=0.7, relwidth=0.7, relheight=0.15)
 
     def display_patientform(self):
         run_patientform(self)
 
     def display_assignbed(self):
-        window = AssignBed(self, self.queue)
-        window.grab_set()
+        msg = messagebox.askquestion("Assign Bed", "Are you sure?")
+        if msg == 'yes':
+            window = AssignBed(self, self.queue)
+            window.grab_set()
 
     def _queue_test(self):
         cian = Patient("cian", "mcdonald", 2)
