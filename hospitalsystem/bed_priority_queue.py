d89da0b7937f5b7ae8471332e90632d186aa175f
CianMcDonald <cianmcdonald2001@gmail.com>
2021-11-06 23:59:19 +0000

    add remove_from_queue

--- hospitalsystem/bed_priority_queue.py
+++ hospitalsystem/bed_priority_queue.py
@@ -9,7 +9,10 @@ class PriorityHeap:
         heapq.heappush(self._queue, patient)
 
     def pop_queue(self):
-        return heapq.heappop(self._queue)
+        try:
+            return heapq.heappop(self._queue)
+        except IndexError:
+            return "Error: Empty queue"
 
     def length_of_queue(self):
         return len(self._queue)
@@ -19,4 +22,17 @@ class PriorityHeap:
             return self._queue[0]
         except IndexError:
             return "Error: Empty queue"
+    
+    def remove_from_queue(self, names):
+        """
+            Takes list of "fname sname" removes patients from queue that match
+        """
+        for pateint in names:
+            name = pateint.split()
+            to_remove = Patient(name[0], name[1], 0)
+            self._queue.remove(to_remove)
+        heapq.heapify(self._queue)
+        print(self._queue)
+
+
 
